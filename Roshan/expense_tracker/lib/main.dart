3d52afccef519c1ca9d9a4bcd663821b7b6da984
Roshan Daivajna <roshan.daivajna@gmail.com>
2020-06-20 01:04:51 +0530

    Updated current project view

--- Roshan/expense_tracker/lib/main.dart
+++ Roshan/expense_tracker/lib/main.dart
@@ -26,10 +26,23 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 3;
+  int _counter = 1;
+  int _id = 0;
+
+  List<Map<String, String>> _expenseList = [];
+
+  void _addExpense() {
+    _expenseList.insert(0, {
+      'id': _id.toString(),
+      'amount': '100',
+      'date': DateTime.now().toString(),
+    });
+  }
 
   void _incrementCounter() {
     setState(() {
+      _id++;
+      _addExpense();
       _counter++;
     });
   }
@@ -39,6 +52,47 @@ class _MyHomePageState extends State<MyHomePage> {
     return Scaffold(
       appBar: AppBar(
         title: Text(widget.title),
+        actions: <Widget>[
+          PopupMenuButton<String>(
+              onSelected: (value) {
+                if (value == 'finish') {
+                  showDialog(
+                    context: context,
+                    builder: (context) {
+                      return AlertDialog(
+                        title: Text('End Project'),
+                        content: Text(
+                          'Are you sure you want to end tracking expenses of this project?',
+                        ),
+                        actions: <Widget>[
+                          FlatButton(
+                            onPressed: () => Navigator.of(context).pop(),
+                            child: Text('CANCEL'),
+                          ),
+                          FlatButton(
+                            onPressed: () {
+                              Navigator.of(context).pop();
+                              Navigator.push(
+                                  context,
+                                  MaterialPageRoute(
+                                    builder: (context) => MyApp(),
+                                  ));
+                            },
+                            child: Text('CONFIRM'),
+                          ),
+                        ],
+                      );
+                    },
+                  );
+                }
+              },
+              itemBuilder: (BuildContext context) => <PopupMenuEntry<String>>[
+                    const PopupMenuItem<String>(
+                      value: 'finish',
+                      child: Text('End Project'),
+                    ),
+                  ]),
+        ],
       ),
       body: ListView.separated(
         itemBuilder: (BuildContext context, int index) {
@@ -47,18 +101,118 @@ class _MyHomePageState extends State<MyHomePage> {
           } else {
             return Container(
               height: 50,
-              child: Text('$index'),
+              child: Text('${_expenseList[index - 1].toString()}'),
             );
           }
         },
         separatorBuilder: (BuildContext context, int index) => const Divider(),
         itemCount: _counter,
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
+      floatingActionButton: MyFloatingActionButton(
+        incrementCounter: _incrementCounter,
       ),
+    );
+  }
+}
+
+class MyFloatingActionButton extends StatelessWidget {
+  final VoidCallback incrementCounter;
+
+  MyFloatingActionButton({Key key, this.incrementCounter}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return FloatingActionButton(
+      onPressed: () {
+        incrementCounter();
+        showModalBottomSheet(
+            context: context,
+            // backgroundColor: Colors.red[50],
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.only(
+                topLeft: Radius.circular(10.0),
+                topRight: Radius.circular(10.0),
+              ),
+            ),
+            builder: (context) => Container(
+                  height: 150,
+                  child: Column(
+                    children: <Widget>[
+                      Expanded(
+                        child: Center(
+                          child: Text(
+                            "Upload New Expense",
+                            style: TextStyle(
+                              fontSize: 17,
+                              fontWeight: FontWeight.w500,
+                            ),
+                          ),
+                        ),
+                        flex: 1,
+                      ),
+                      Expanded(
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceAround,
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          children: <Widget>[
+                            Column(
+                              mainAxisAlignment: MainAxisAlignment.start,
+                              children: <Widget>[
+                                Icon(
+                                  Icons.camera_alt,
+                                  size: 30,
+                                  color: Color.fromRGBO(0, 0, 0, 0.7),
+                                ),
+                                Text(
+                                  "Scan",
+                                  style: TextStyle(
+                                    color: Color.fromRGBO(0, 0, 0, 0.7),
+                                  ),
+                                )
+                              ],
+                            ),
+                            Column(
+                              mainAxisAlignment: MainAxisAlignment.start,
+                              children: <Widget>[
+                                Icon(
+                                  Icons.mic,
+                                  size: 30,
+                                  color: Color.fromRGBO(0, 0, 0, 0.7),
+                                ),
+                                Text(
+                                  "Voice",
+                                  style: TextStyle(
+                                    color: Color.fromRGBO(0, 0, 0, 0.7),
+                                  ),
+                                )
+                              ],
+                            ),
+                            Column(
+                              mainAxisAlignment: MainAxisAlignment.start,
+                              children: <Widget>[
+                                Icon(
+                                  Icons.assignment,
+                                  size: 30,
+                                  color: Color.fromRGBO(0, 0, 0, 0.7),
+                                ),
+                                Text(
+                                  "Manual",
+                                  style: TextStyle(
+                                    color: Color.fromRGBO(0, 0, 0, 0.7),
+                                  ),
+                                )
+                              ],
+                            ),
+                          ],
+                        ),
+                        flex: 1,
+                      ),
+                    ],
+                  ),
+                ));
+      },
+      tooltip: 'Add Expense',
+      child: Icon(Icons.add),
     );
   }
 }
@@ -71,32 +225,71 @@ class SummaryCard extends StatefulWidget {
 class _SummaryCardState extends State<SummaryCard> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Row(
-        children: <Widget>[
-          Column(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              Text('Budget'),
-              Text(
-                '1000',
-                style: TextStyle(color: Colors.green),
-              ),
-            ],
+    return Padding(
+      padding: EdgeInsets.all(15.0),
+      child: Container(
+        height: 120,
+        decoration: BoxDecoration(
+          color: Theme.of(context).scaffoldBackgroundColor,
+          borderRadius: BorderRadius.circular(10),
+          border: Border.all(
+            width: 0.1,
+            color: Colors.grey.shade400,
           ),
-          Column(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              Text('Spent'),
-              Text(
-                '500',
-                style: TextStyle(color: Colors.red),
-              ),
-            ],
-          ),
-        ],
+          boxShadow: [
+            BoxShadow(
+              color: Colors.grey.shade300,
+              blurRadius: 1.0,
+              offset: Offset(0, 2),
+            ),
+          ],
+        ),
+        child: Row(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: <Widget>[
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Text(
+                  'Budget',
+                  style: TextStyle(fontSize: 25),
+                ),
+                SizedBox(
+                  height: 5,
+                ),
+                Text(
+                  'Rs. 1000',
+                  style: TextStyle(
+                    color: Colors.green,
+                    fontSize: 18,
+                  ),
+                ),
+              ],
+            ),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Text(
+                  'Spent',
+                  style: TextStyle(fontSize: 25),
+                ),
+                SizedBox(
+                  height: 5,
+                ),
+                Text(
+                  'Rs. 500',
+                  style: TextStyle(
+                    color: Colors.red,
+                    fontSize: 18,
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
       ),
     );
   }
