32113ab1d0ba446f1b8b9a0cf612c82f5556ea79
Vishruth Veerendranath <vishruth2002@gmail.com>
2020-06-20 19:08:29 +0530

    "Added Date Selection and Bill Amount field"

--- Vishruth/expense_track_vish/lib/main.dart
+++ Vishruth/expense_track_vish/lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
-//import 'package:intl/intl.dart';
-//import 'package:datetime_picker_formfield/datetime_picker_formfield.dart';
+//import 'dart:async';
 
 void main() => runApp(MyApp());
 
@@ -34,59 +33,122 @@ class _NewExpenseState extends State<NewExpense> {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: <Widget>[
-        ListTile(
-          title: Text(
-            'Category:',
-            style: TextStyle(
-              fontSize: 20,
-              color: Colors.black,
+    return Padding(
+      padding: const EdgeInsets.only(top:25),
+      child: Column(
+        children: <Widget>[
+          ListTile(
+            title: Text(
+              'Category:',
+              style: TextStyle(
+                fontSize: 20,
+                color: Colors.black,
+              ),
+            ),
+            trailing: DropdownButton(
+              value: dropdownValue,
+              items: <String>['Select', 'Food', 'Materials']
+                  .map<DropdownMenuItem<String>>((String value) {
+                return DropdownMenuItem<String>(
+                  value: value,
+                  child: Text(value),
+                );
+              }).toList(),
+              onChanged: (String newValue) {
+                setState(() {
+                  dropdownValue = newValue;
+                });
+              },
             ),
           ),
-          trailing: DropdownButton(
-            items: <String>['Select', 'Food', 'Materials']
-                .map<DropdownMenuItem<String>>((String value) {
-              return DropdownMenuItem<String>(
-                value: value,
-                child: Text(value),
-              );
-            }).toList(),
-            onChanged: (String newValue) {
-              setState(() {
-                dropdownValue = newValue;
-              });
-            },
+          SizedBox(
+            height: 20,
           ),
-        ),
-        SizedBox(
-          height: 20,
-        ),
-        Divider(),
-        ListTile(
-          title: Text(
-            'Date:',
-            style: TextStyle(
-              fontSize: 20,
-              color: Colors.black,
+          Divider(),
+          ListTile(
+            title: Text(
+              'Date:',
+              style: TextStyle(
+                fontSize: 20,
+                color: Colors.black,
+              ),
+            ),
+            trailing: Row(
+              mainAxisSize: MainAxisSize.min,
+              children: <Widget>[
+                Text(
+                  selectedDate == null ? 'Select Date' : selectedDate.toString(),
+                ),
+                RaisedButton(
+                  shape: RoundedRectangleBorder(),
+                  textColor: Colors.white,
+                  child: Text('SELECT DATE'),
+                  color: Colors.red,
+                  onPressed: () {
+                    showDatePicker(
+                      initialEntryMode: DatePickerEntryMode.input,
+                      context: context,
+                      initialDate:
+                          selectedDate == null ? DateTime.now() : selectedDate,
+                      firstDate: DateTime(2001),
+                      lastDate: DateTime(2030),
+                    ).then((date) {
+                      setState(() {
+                        selectedDate = date;
+                      });
+                    });
+                  },
+                ),
+              ],
             ),
           ),
-          // trailing: Row(
-          //   children: <Widget>[
-          //     // DateTimeField(
-          //     //   format: format,
-          //     //   onShowPicker: (context, currentValue) {
-          //     //     return showDatePicker(
-          //     //         context: context,
-          //     //         firstDate: DateTime(1900),
-          //     //         initialDate: currentValue ?? DateTime.now(),
-          //     //         lastDate: DateTime(2100));
-          //     //   },
-          //     // ),
-          //   ],
-          // ),
-        ),
-      ],
+          SizedBox(
+            height: 20,
+          ),
+          Divider(),
+          ListTile(
+            title: Text(
+              'Bill Amount:',
+              style: TextStyle(fontSize: 20),
+            ),
+            trailing: Container(
+              width: 200,
+              child: TextField(),
+            ),
+          )
+        ],
+      ),
     );
   }
 }
+
+// trailing: Row(
+//   children: <Widget>[
+//     // DateTimeField(
+//     //   format: format,
+//     //   onShowPicker: (context, currentValue) {
+//     //     return showDatePicker(
+//     //         context: context,
+//     //         firstDate: DateTime(1900),
+//     //         initialDate: currentValue ?? DateTime.now(),
+//     //         lastDate: DateTime(2100));
+//     //   },
+//     // ),
+//   ],
+// ),
+
+// trailing: RaisedButton(
+//             onPressed: () {
+//               showDatePicker(
+//                 context: context,
+//                 initialDate: DateTime.now(),
+//                 firstDate: DateTime(2001),
+//                 lastDate: DateTime(2030),
+//                 fieldLabelText: selectedDate.toString(),
+//               ).then((date) {
+//                 setState(() {
+//                   selectedDate = date;
+//                 });
+//               });
+//             },
+//           ),
