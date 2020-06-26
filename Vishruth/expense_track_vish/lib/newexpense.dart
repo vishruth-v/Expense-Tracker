dc75d318fe0e9c096b77b353375ad0f8f5bd2163
Vishruth Veerendranath <vishruth2002@gmail.com>
2020-06-26 20:43:34 +0530

    Updated View of NewExpense page and added attachment of image

--- Vishruth/expense_track_vish/lib/newexpense.dart
+++ Vishruth/expense_track_vish/lib/newexpense.dart
@@ -1,11 +1,15 @@
-//import 'package:flutter/foundation.dart';
+import 'dart:async';
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:intl/intl.dart';
-//import 'package:camera/camera.dart';
-//import 'dart:async';
+import 'package:image_picker/image_picker.dart';
+
 
 //void main() => runApp(MyApp());
+
+enum AttachOption { camera, gallery }
 
 class NewExpenseMain extends StatelessWidget {
   @override
@@ -37,6 +41,21 @@ class _NewExpenseState extends State<NewExpense> {
   double amount;
   bool _validate = false;
   String description;
+  List<String> _categories = [
+    'Select',
+    'Travel',
+    'Food',
+    'Materials',
+    'Conveyence'
+  ];
+
+  List<String> _attachOptions = ['Camera', 'Gallery'];
+
+  String attachOption;
+
+  File _image;
+  final picker = ImagePicker();
+  AttachOption _attachoption;
   //final format = DateFormat("yyyy-MM-dd");
 
   @override
@@ -63,13 +82,8 @@ class _NewExpenseState extends State<NewExpense> {
                     DropdownButton(
                       underline: Container(height: 2, color: Colors.red),
                       value: dropdownValue,
-                      items: <String>[
-                        'Select',
-                        'Travel',
-                        'Food',
-                        'Materials',
-                        'Conveyence'
-                      ].map<DropdownMenuItem<String>>((String value) {
+                      items: _categories
+                          .map<DropdownMenuItem<String>>((String value) {
                         return DropdownMenuItem<String>(
                           value: value,
                           child: Text(value),
@@ -84,30 +98,53 @@ class _NewExpenseState extends State<NewExpense> {
                   ],
                 ),
                 Divider(),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: <Widget>[
-                    Text(
-                      'Bill No:',
-                      style: TextStyle(fontSize: 20),
-                    ),
-                    Container(
-                      width: 200,
-                      child: TextField(
-                        onChanged: (String text) {
-                          setState(() {
-                            billno = text;
-                          });
-                        },
-                        style: TextStyle(fontSize: 20),
-                      ),
-                    ),
-                  ],
+                TextField(
+                  decoration: InputDecoration(
+                      labelText: 'Bill Number:',
+                      labelStyle: TextStyle(color: Colors.black, fontSize: 20)),
+                  onChanged: (String text) {
+                    setState(() {
+                      billno = text;
+                    });
+                  },
+                  style: TextStyle(fontSize: 22),
                 ),
                 Divider(
                   height: 30,
                 ),
-                Row(
+
+                //USE INKWELL WIDGET OR REPLACE WITH COMMENTED SECTION TO REVERT TO BUTTON SELECTION
+                //INKWELL WAS USED TO PREVENT OVERFLOW
+                InkWell(
+                  child: InputDecorator(
+                    decoration: InputDecoration(
+                      labelText: 'Date:',
+                    ),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: <Widget>[
+                        Text(
+                          selectedDate == null
+                              ? 'Select Date'
+                              //: selectedDate.toString(),
+                              : //DateFormat('d/M/yyyy')
+                              DateFormat.yMMMd()
+                                  .format(selectedDate)
+                                  .toString(),
+                          style: TextStyle(fontSize: 20),
+                        ),
+                        Icon(
+                          Icons.calendar_today,
+                          color: Colors.red,
+                        )
+                      ],
+                    ),
+                  ),
+                  onTap: _datePicker,
+                ),
+
+                //USE THIS SECTION BY REPLACING INKWELL IF WANT TO REVERT TO BUTTON FOR SELECT DATE
+                /*Row(
                   mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   children: <Widget>[
                     Text(
@@ -124,10 +161,11 @@ class _NewExpenseState extends State<NewExpense> {
                           selectedDate == null
                               ? 'Select Date'
                               //: selectedDate.toString(),
-                              : DateFormat('d/M/yyyy')
+                              : //DateFormat('d/M/yyyy')
+                                DateFormat.yMMMd()
                                   .format(selectedDate)
                                   .toString(),
-                          style: TextStyle(fontSize: 18),
+                          style: TextStyle(fontSize: 20),
                         ),
                         SizedBox(
                           width: 10,
@@ -135,86 +173,53 @@ class _NewExpenseState extends State<NewExpense> {
                         RaisedButton(
                           shape: RoundedRectangleBorder(),
                           textColor: Colors.white,
-                          child: Text('SELECT DATE'),
+                          child: Text('CHANGE DATE'),
                           color: Colors.red,
-                          onPressed: () {
-                            showDatePicker(
-                                context: context,
-                                initialDate: selectedDate == null
-                                    ? DateTime.now()
-                                    : selectedDate,
-                                firstDate: DateTime(2001),
-                                lastDate: DateTime(2030),
-                                builder: (BuildContext context, Widget child) {
-                                  return Theme(
-                                    child: child,
-                                    data: ThemeData(primarySwatch: Colors.red),
-                                  );
-                                }).then((date) {
-                              setState(() {
-                                selectedDate = date;
-                              });
-                            });
-                          },
+                          onPressed: _datePicker,
                         ),
                       ],
                     ),
                   ],
-                ),
+                ),*/
+
                 Divider(height: 15),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: <Widget>[
-                    Text(
-                      'Bill Amount:',
-                      style: TextStyle(fontSize: 20),
-                    ),
-                    Container(
-                      width: 200,
-                      child: TextField(
-                        onChanged: (text) {
-                          setState(() {
-                            _validate = false;
-                            amount = double.tryParse(text);
-                            if (amount == null) {
-                              _validate = true;
-                            }
-                          });
-                        },
-                        style: TextStyle(fontSize: 20),
-                        keyboardType: TextInputType.numberWithOptions(
-                            decimal: true, signed: false),
-                        decoration: InputDecoration(
-                          prefixText: "\u20B9  ",
-                          errorText: _validate ? 'Invalid Input' : null,
-                        ),
-                      ),
-                    ),
-                  ],
+                TextField(
+                  onChanged: (text) {
+                    setState(() {
+                      _validate = false;
+                      amount = double.tryParse(text);
+                      if (amount == null) {
+                        _validate = true;
+                      }
+                    });
+                  },
+                  style: TextStyle(fontSize: 22),
+                  keyboardType: TextInputType.numberWithOptions(
+                      decimal: true, signed: false),
+                  decoration: InputDecoration(
+                    labelText: 'Bill Amount:',
+                    labelStyle: TextStyle(fontSize: 20, color: Colors.black),
+                    prefixText: "\u20B9  ",
+                    errorText: _validate ? 'Invalid Input' : null,
+                  ),
                 ),
                 Divider(height: 30),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: <Widget>[
-                    Text(
-                      'Description:',
-                      style: TextStyle(fontSize: 20),
-                    ),
-                    Container(
-                      width: 200,
-                      child: TextField(
-                        maxLines: 5,
-                        keyboardType: TextInputType.multiline,
-                        textInputAction: TextInputAction.newline,
-                        decoration: InputDecoration(fillColor: Colors.grey[50], filled: true, hintText: 'Enter Description'),
-                        onChanged: (String text) {
-                          setState(() {
-                            description = text;
-                          });
-                        },
-                      ),
-                    ),
-                  ],
+                TextField(
+                  maxLines: 5,
+                  keyboardType: TextInputType.multiline,
+                  textInputAction: TextInputAction.newline,
+                  decoration: InputDecoration(
+                    fillColor: Colors.grey[100],
+                    filled: true,
+                    hintText: 'Enter Description',
+                    hintStyle: TextStyle(fontSize: 18),
+                  ),
+                  onChanged: (String text) {
+                    setState(() {
+                      description = text;
+                    });
+                  },
+                  style: TextStyle(fontSize: 18),
                 ),
               ],
             ),
@@ -222,73 +227,55 @@ class _NewExpenseState extends State<NewExpense> {
           Row(
             mainAxisAlignment: MainAxisAlignment.center,
             children: <Widget>[
-              FloatingActionButton(
-                onPressed: null,
-                child: Icon(Icons.attach_file),
-                backgroundColor: Colors.red,
+              PopupMenuButton<AttachOption>(
+                child: FloatingActionButton(
+                  child: Icon(Icons.attach_file),
+                  onPressed: null,
+                  backgroundColor: Colors.red,
+                ),
+                onSelected: (AttachOption result) {
+                  setState(() {
+                    _attachoption = result;
+                  });
+                  attach();
+                },
+                itemBuilder: (BuildContext context) =>
+                    <PopupMenuEntry<AttachOption>>[
+                  PopupMenuItem<AttachOption>(
+                    value: AttachOption.camera,
+                    child: Row(
+                      children: <Widget>[
+                        Icon(
+                          Icons.camera_alt,
+                          color: Colors.red,
+                        ),
+                        SizedBox(
+                          width: 5,
+                        ),
+                        Text('Camera'),
+                      ],
+                    ),
+                  ),
+                  PopupMenuItem<AttachOption>(
+                    value: AttachOption.gallery,
+                    child: Row(
+                      children: <Widget>[
+                        Icon(
+                          Icons.photo,
+                          color: Colors.red,
+                        ),
+                        SizedBox(
+                          width: 5,
+                        ),
+                        Text('Gallery'),
+                      ],
+                    ),
+                  )
+                ],
               ),
               SizedBox(width: 30),
               FloatingActionButton(
-                onPressed: () {
-                  if (_validate == false && dropdownValue != 'Select') {
-                    return showDialog(
-                        context: context,
-                        builder: (BuildContext context) {
-                          return AlertDialog(
-                            title: Text('Submit New expense?',
-                                style: TextStyle(fontSize: 25)),
-                            content: SingleChildScrollView(
-                              child: ListBody(
-                                children: <Widget>[
-                                  Text('Bill amount is: $amount'),
-                                  Text('For Category $dropdownValue'),
-                                  Text('Do you confirm?'),
-                                ],
-                              ),
-                            ),
-                            contentTextStyle:
-                                TextStyle(fontSize: 18, color: Colors.black),
-                            actions: <Widget>[
-                              FlatButton(
-                                  child: Text(
-                                    'CANCEL',
-                                    style: TextStyle(
-                                      fontSize: 18,
-                                      color: Colors.red,
-                                    ),
-                                  ),
-                                  //color: Colors.red,
-                                  onPressed: () {}),
-                              FlatButton(
-                                child: Text('SUBMIT',
-                                    style: TextStyle(fontSize: 18)),
-                                color: Colors.red,
-                                onPressed: () {},
-                              )
-                            ],
-                          );
-                        });
-                  } else {
-                    return showDialog(
-                        context: context,
-                        builder: (BuildContext context) {
-                          return AlertDialog(
-                            title: Text('Invalid Expense Details',
-                                style: TextStyle(fontSize: 25)),
-                            content: SingleChildScrollView(
-                              child: ListBody(
-                                children: <Widget>[
-                                  Text('Category or Bill Amount is Invalid'),
-                                  Text('Try Again'),
-                                ],
-                              ),
-                            ),
-                            contentTextStyle:
-                                TextStyle(fontSize: 18, color: Colors.black),
-                          );
-                        });
-                  }
-                },
+                onPressed: _submitDialog,
                 child: Icon(Icons.done),
                 backgroundColor: Colors.red,
               ),
@@ -298,5 +285,110 @@ class _NewExpenseState extends State<NewExpense> {
       ),
     );
   }
-}
 
+  void _datePicker() {
+    showDatePicker(
+        context: context,
+        initialDate: selectedDate == null ? DateTime.now() : selectedDate,
+        firstDate: DateTime(2001),
+        lastDate: DateTime(2030),
+        builder: (BuildContext context, Widget child) {
+          return Theme(
+            child: child,
+            data: ThemeData(primarySwatch: Colors.red),
+          );
+        }).then((date) {
+      setState(() {
+        selectedDate = date;
+      });
+    });
+  }
+
+  void _submitDialog() {
+    if (_validate == false &&
+        dropdownValue != 'Select' &&
+        selectedDate != null) {
+      showDialog(
+          context: context,
+          builder: (BuildContext context) {
+            return AlertDialog(
+              title:
+                  Text('Submit New expense?', style: TextStyle(fontSize: 25)),
+              content: SingleChildScrollView(
+                child: ListBody(
+                  children: <Widget>[
+                    Text('Bill amount is: $amount'),
+                    Text('For Category $dropdownValue'),
+                    Text('Do you confirm?'),
+                  ],
+                ),
+              ),
+              contentTextStyle: TextStyle(fontSize: 18, color: Colors.black),
+              actions: <Widget>[
+                FlatButton(
+                    child: Text(
+                      'CANCEL',
+                      style: TextStyle(
+                        fontSize: 18,
+                        color: Colors.red,
+                      ),
+                    ),
+                    //color: Colors.red,
+                    onPressed: () {}),
+                FlatButton(
+                  child: Text('SUBMIT', style: TextStyle(fontSize: 18)),
+                  color: Colors.red,
+                  onPressed: () {},
+                )
+              ],
+            );
+          });
+    } else {
+      showDialog(
+          context: context,
+          builder: (BuildContext context) {
+            return AlertDialog(
+              title: Text('Invalid Expense Details',
+                  style: TextStyle(fontSize: 25)),
+              content: SingleChildScrollView(
+                child: ListBody(
+                  children: <Widget>[
+                    Text('Category, Date or Bill Amount is Invalid.'),
+                    Text('Try Again'),
+                  ],
+                ),
+              ),
+              contentTextStyle: TextStyle(fontSize: 18, color: Colors.black),
+            );
+          });
+    }
+  }
+
+  Future getImageCamera() async {
+    final pickedFile = await picker.getImage(source: ImageSource.camera);
+
+    if (pickedFile != null) {
+      setState(() {
+        _image = File(pickedFile.path);
+      });
+    }
+  }
+
+  Future getImageGallery() async {
+    final pickedFile = await picker.getImage(source: ImageSource.gallery);
+
+    if (pickedFile != null) {
+      setState(() {
+        _image = File(pickedFile.path);
+      });
+    }
+  }
+
+  void attach() {
+    if (_attachoption == AttachOption.camera) {
+      getImageCamera();
+    } else if (_attachoption == AttachOption.gallery) {
+      getImageGallery();
+    }
+  }
+}
